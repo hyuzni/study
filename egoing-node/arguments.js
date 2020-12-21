3519775dcee8cf6f187c1237120d57398b928d6f
hyuzni <hyuzni@gmail.com>
2020-12-21 17:44:46 +0900

    commit node js

---
+++ egoing-node/arguments.js
@@ -0,0 +1,9 @@
+var args = process.argv
+console.log(args)
+
+// node arguments.js hyuzni 12345
+
+// 'C:\\Program Files\\nodejs\\node.exe',
+// 'C:\\Users\\PI190037\\Desktop\\hyuzni\\project\\study\\egoing-node\\arguments.js',
+// 'hyuzni',
+// '12345'
--- egoing-node/main.js
+++ egoing-node/main.js
@@ -2,47 +2,61 @@ var http = require("http")
 var fs = require("fs") // file system
 var url = require("url")
 
-var app = http.createServer(function (request, response) {
-  var queryData = url.parse(request.url, true).query
-  var title = queryData.id
+function templateHTML(title, list, content) {
+  var template = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>WEB | ${title}</title>
+      <meta charset="utf-8" />
+    </head>
+    <body>
+      <h1><a href="/">${title}</a></h1>
+      ${list}
+      <h2>${title}</h2>
+      ${content}
+    </body>
+  </html>`
 
-  if (title == null) {
-    title = "welcome"
-  }
-  if (request.url == "/") {
-    request.url = "/index.html"
-  }
-  if (request.url == "/favicon.ico") {
-    // return response.writeHead(404)
-    response.writeHead(404)
-    response.end()
+  return template
+}
+
+function templateList(files) {
+  if (files.length === 0) {
     return
   }
-  fs.readFile(`data/${queryData.id}`, "utf-8", (err, description) => {
-    var template = `
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>WEB | ${title}</title>
-          <meta charset="utf-8" />
-        </head>
-        <body>
-          <h1><a href="/">${title}</a></h1>
-          <ul>
-            <li><a href="?id=html">HTML</a></li>
-            <li><a href="?id=css">CSS</a></li>
-            <li><a href="?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          ${description}
-        </body>
-      </html>`
-
-    response.writeHead(200)
-    response.end(template)
+  var list = `<ul>`
+  files.forEach((file) => {
+    list += `<li><a href="?id=${file}">${file}</a></li>`
   })
+  list += `</ul>`
 
+  return list
+}
+var app = http.createServer(function (request, response) {
+  var _url = request.url
+  var queryData = url.parse(_url, true).query
+  var pathname = url.parse(_url, true).pathname
+  var title = queryData.id
+
+  if (pathname === "/") {
+    fs.readdir("data", (err, files) => {
+      var list = templateList(files)
+
+      fs.readFile(`data/${queryData.id}`, "utf-8", (err, content) => {
+        if (title === undefined) {
+          title = "welcome"
+          content = "hello, node js"
+        }
+        response.writeHead(200)
+        response.end(templateHTML(title, list, content))
+      })
+    })
+  } else {
+    response.writeHead(400)
+    response.end("not found")
+  }
   // response.end(fs.readFileSync(__dirname + "/" + title))
-  //   response.end(fs.readFileSync(__dirname + "/" + queryData.id))
+  // response.end(fs.readFileSync(__dirname + "/" + queryData.id))
 })
 app.listen(4000)
