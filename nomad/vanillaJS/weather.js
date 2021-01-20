55e047b67e4ffb1c040b78d517569c0cf52c1bd0
hyuzni <hyuzni@gmail.com>
2021-01-20 16:28:53 +0900

    finish vanillaJS study

--- nomad/vanillaJS/weather.js
+++ nomad/vanillaJS/weather.js
@@ -1,6 +1,19 @@
 const COORDS = "coords"
-
 const API_KEY = "7f02ab5bd27ffaebf7dc5fca71eb35b3"
+const weather = document.querySelector(".js-weather")
+
+function getWeather(lat, lon) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (json) {
+      const temperature = json.main.temp
+      const place = json.name
+      weather.innerText = `${temperature} @ ${place}`
+    })
+}
+
 function saveCoords(obj) {
   localStorage.setItem("coords", JSON.stringify(obj))
 }
@@ -12,6 +25,7 @@ function handleGeoSuccess(position) {
     longitude,
   }
   saveCoords(coordsObj)
+  getWeather(latitude, longitude)
 }
 function handleGeoError(position) {
   console.log(position)
@@ -24,6 +38,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords()
   } else {
+    const parseCoords = JSON.parse(loadedCoords)
+    getWeather(parseCoords.latitude, parseCoords.longitude)
   }
 }
 function init() {
